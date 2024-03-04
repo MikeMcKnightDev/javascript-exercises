06a07447138acf1de67e36099dab99e8bbd5f0a9
Mike McKnight <142824213+MikeMcKnightDev@users.noreply.github.com>
2024-03-04 13:08:38 -0500

    Complete js exercise 6.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let isLeapYear;
+    if(year % 100 === 0 && year % 400 !== 0){
+        return isLeapYear = false;
+    } else if(year % 4 === 0){
+        return isLeapYear = true;
+    } else if(year % 4 !== 0){
+        return isLeapYear = false;
+    }
 
+    
 };
 
 // Do not edit below this line
